ef14e8ab365615f7dea5f0109bd6f979bf562c2f
zackvpro <shailene.fly@gmail.com>
2026-02-27 16:08:40 +0800

    Quartz sync: Feb 27, 2026, 4:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f5f2e9",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
